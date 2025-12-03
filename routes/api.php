e672772d9b4eeb263c772eabe93140db01e5e91c
OshadaPerera <Oshadhakavinduperera1@gmail.com>
2025-12-03 22:16:44 +0530

    api unwanted routes removed

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
 
 // Public API routes - no authentication required
 Route::withoutMiddleware(['auth:sanctum'])->group(function () {
